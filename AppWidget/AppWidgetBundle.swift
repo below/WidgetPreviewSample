772511700a0bc942c93064ac425b2b3d7ac5e94f
Alexander von Below <alex@vonbelow.com>
2023-06-11 18:12:27 +0200

    Added an App Widget

---
+++ AppWidget/AppWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  AppWidgetBundle.swift
+//  AppWidget
+//
+//  Created by Alexander von Below on 11.06.23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct AppWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        AppWidget()
+    }
+}
